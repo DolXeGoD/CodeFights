422dfeeec846c26710e1f70f0b0049b507239941
DolXeGoD <dolxegod@gmail.com>
2018-05-20 20:42:03 +0900

    first commit

---
+++ com/dolxegod/codefights/Add.java
@@ -0,0 +1,15 @@
+package com.dolxegod.codefights;
+
+public class Add {
+    public static void main(String[] args){
+        add(1, 2);
+        add(0, 1000);
+        add(2, -39);
+        add(99, 100);
+    }
+
+    public static int add(int param1, int param2) {
+        int result = param1 + param2;
+        return result;
+    }
+}
---
+++ com/dolxegod/codefights/CenturyFromYear.java
@@ -0,0 +1,25 @@
+package com.dolxegod.codefights;
+
+import java.util.Scanner;
+
+public class CenturyFromYear {
+    public static void main(String[] args) {
+        System.out.println(centuryFromYear(1905));
+        System.out.println(centuryFromYear(1700));
+        System.out.println(centuryFromYear(1988));
+        System.out.println(centuryFromYear(2000));
+        System.out.println(centuryFromYear(2001));
+    }
+
+    public static int centuryFromYear(int year){
+        int result;
+        if(1<= year && 2018 >= year){
+            if(year % 100 == 0){
+                return year/100;
+            } else{
+                return (year/100)+1;
+            }
+        }
+        return 0;
+    }
+}
