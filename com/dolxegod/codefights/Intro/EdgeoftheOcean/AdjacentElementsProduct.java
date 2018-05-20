0d386c5f3931f2a5e4d24bfbee220529d4b07450
DolXeGoD <dolxegod@gmail.com>
2018-05-20 21:19:57 +0900

    added solution

---
+++ com/dolxegod/codefights/Intro/EdgeoftheOcean/AdjacentElementsProduct.java
@@ -0,0 +1,23 @@
+package com.dolxegod.codefights.Intro.EdgeoftheOcean;
+
+public class AdjacentElementsProduct {
+    public static void main(String args[]){
+        int[] a = new int[]{3, 6, -2, -5, 7, 3};
+        adjacentElementsProduct(a);
+    }
+
+    private static int adjacentElementsProduct(int[] inputArray) {
+        int length = inputArray.length;
+        int result;
+        int comp;
+
+        result = inputArray[0] * inputArray[1];
+        for(int i=1; i<length-1; i++){
+            comp = inputArray[i] * inputArray[i+1];
+            if(comp > result){
+                result = comp;
+            }
+        }
+        return result;
+    }
+}
